055009bf09d4b86c6c87d0d9e0883a16b1d74fed
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-03-15 11:52:01 +0000

    WIP

---
+++ haskell/Main.hs
@@ -0,0 +1,226 @@
+module Main (main) where
+
+import Bluefin.Eff
+import Bluefin.Exception
+import Bluefin.IO
+import Control.Monad
+import Data.ByteString.Lazy.Char8 qualified as LBS
+import Data.String
+import System.Environment
+import System.Process.Typed
+
+main :: IO ()
+main = runEff_ $ \io -> handle (effIO io . putStrLn) $ \ex -> do
+  let r s = fmap snd (effIO io (readProcessStdout (fromString s)))
+  let rThrow s = do
+        (exitCode, stdout) <- effIO io (readProcessStdout (fromString s))
+        case exitCode of
+          failure@(ExitFailure {}) -> throw ex (show failure)
+          ExitSuccess -> pure stdout
+  let echoN = effIO io . putStr
+  let echo = effIO io . putStrLn
+
+  (arg1, arg2) <-
+    effIO io getArgs >>= \case
+      [arg1, arg2] -> pure (arg1, arg2)
+      _ -> throw ex "Expected two arguments"
+
+  branch <- fmap LBS.unpack (r "git symbolic-ref --quiet --short HEAD")
+  current <- fmap LBS.unpack (rThrow "git rev-parse HEAD")
+  currentShort <- fmap LBS.unpack (rThrow "git rev-parse --short HEAD")
+
+  let branchOrCurrentShort =
+        if not (null branch)
+          then
+            branch
+          else currentShort
+
+  let combinedProvided = arg2
+  combined <-
+    fmap LBS.unpack (rThrow ("git rev-parse " <> combinedProvided))
+  combinedShort <- rThrow ("git rev-parse --short " <> combinedProvided)
+  let handler = arg1
+
+  let throwFailed s msg =
+        effIO io (runProcess (fromString s)) >>= \case
+          ExitFailure {} -> throw ex msg
+          ExitSuccess -> pure ()
+
+  throwFailed
+    ("git merge-base --is-ancestor " <> combinedProvided <> " " <> current)
+    (combinedProvided <> " is not an ancestor of " <> branchOrCurrentShort)
+
+  throwFailed
+    "git diff --quiet"
+    ( "The repo has uncommitted changes.  "
+        <> "Stash, commit or reset them and then try again."
+    )
+
+  throwFailed
+    "git diff --quiet"
+    ( "The repo has uncommitted changes.  "
+        <> "Stash, commit or reset them and then try again."
+    )
+
+  effIO
+    io
+    ( runProcess
+        ( fromString ("git rev-parse --verify --quiet " <> combined <> "^2")
+        )
+    )
+    >>= \case
+      ExitSuccess -> throw ex (combinedProvided <> " is a merge commit.  Cannot split.")
+      ExitFailure {} -> pure ()
+
+  combinedParent <-
+    rThrow ("git rev-parse " <> combined <> "^")
+  combinedParentShort <-
+    rThrow ("git rev-parse --short " <> combined <> "^")
+
+  echoN "checkout ..."
+  _ <- rThrow ("git checkout --quiet " <> combined)
+  echoN "reset..."
+  _ <- rThrow ("git reset --quiet " <> LBS.unpack combinedParent)
+  echo "done"
+
+  echoN "You were on "
+  if not (null branch)
+    then echoN ("branch " <> branch <> " (" <> currentShort <> ")")
+    else echoN currentShort
+
+  echo "You wanted to split the commit"
+  echo ""
+  _ <- rThrow ("git show --no-patch --pretty=short " <> combined)
+
+  echo ""
+  echoN
+    ( "I'm now on "
+        <> LBS.unpack combinedShort
+        <> "'s parent ("
+        <> LBS.unpack combinedParentShort
+        <> ")"
+    )
+  echo ("I'm going to drop you into your chosen handler: " <> handler)
+  echoN "Please make any number of commits and then exit the handler with "
+  echo "exit code 0"
+
+  effIO io (runProcess (fromString handler)) >>= \case
+    ExitSuccess -> pure ()
+    ExitFailure {} -> do
+      afterFailedHandler <- rThrow "git rev-parse --short HEAD"
+      _ <- rThrow "git reset --quiet --hard"
+      echo
+        ( "The handler failed at "
+            <> LBS.unpack afterFailedHandler
+            <> ".  Retutrning to "
+            <> branchOrCurrentShort
+            <> "."
+        )
+      let returnTo =
+            if not (null branch)
+              then
+                branch
+              else current
+      _ <- rThrow ("git checkout --force --quiet \"" <> returnTo <> "\"")
+      throw ex ""
+
+  afterHandler <- rThrow "git rev-parse HEAD"
+  afterHandlerShort <- rThrow "git rev-parse --short HEAD"
+
+  echoN "reset..."
+  _ <- rThrow ("git reset --quiet --hard " <> LBS.unpack afterHandler)
+
+  echoN "checkout..."
+  _ <- rThrow ("git checkout --quiet --force " <> combined)
+
+  echoN "reset..."
+  _ <- rThrow ("git reset --quiet --force " <> combined)
+
+  combinedSubject <- rThrow ("git diff-tree -s --pretty=%s " <> combined)
+  combinedBody <- rThrow ("git diff-tree -s --pretty=%b " <> combined)
+
+  echoN "commit..."
+  _ <-
+    rThrow
+      ( "git commit --allow-empty --quiet -m \""
+          <> LBS.unpack combinedSubject
+          <> "\" -m \""
+          <> LBS.unpack combinedBody
+          <> "\""
+      )
+
+  restOfCombined <- rThrow "git rev-parse HEAD"
+  -- Check 2 equality
+  echoN "checking equality..."
+  _ <-
+    rThrow
+      ( "git diff --exit-code "
+          <> LBS.unpack restOfCombined
+          <> " "
+          <> combined
+      )
+
+  echoN "rebase..."
+  _ <-
+    rThrow
+      ( "git rebase --quiet --onto "
+          <> LBS.unpack restOfCombined
+          <> " "
+          <> combined
+      )
+
+  finished <- rThrow "git rev-parse HEAD"
+  finishedShort <- rThrow "git rev-parse --short HEAD"
+  let branchOrFinishedShort =
+        if not (null branch)
+          then branch
+          else
+            LBS.unpack finishedShort
+
+  -- Check 3 equality
+  echoN "checking equality..."
+  _ <- rThrow ("git diff --exit-code " <> LBS.unpack finished <> " " <> current)
+
+  when (not (null branch)) $ do
+    echoN "setting branch to history with split..."
+    _ <- rThrow ("git push --quiet --force . HEAD:" <> branch)
+    _ <- rThrow ("git checkout -quiet " <> branch)
+    pure ()
+
+  -- Check 3 e;quality, and we have it checked out
+  echoN "checking equality..."
+  _ <- rThrow ("git diff --exit-code HEAD " <> current)
+
+  echo "done"
+  echo ""
+  echo "Splitting finished successfully!"
+  echo ""
+  if not (null branch)
+    then
+      echoN ("Your branch is " <> branch)
+    else
+      echoN "Your HEAD is detached.  "
+
+  echo
+    ( "It was previously "
+        <> currentShort
+        <> ".  It is now "
+        <> LBS.unpack finishedShort
+        <> "."
+    )
+  echo ""
+  echo "You might want to do exactly one of the following"
+  echo ""
+  echo "* An interactive rebase to reword the remnants of the split"
+  echo ""
+  echo
+    ( "  $ git rebase --interactive "
+        <> LBS.unpack afterHandlerShort
+        <> " "
+        <> branchOrFinishedShort
+    )
+  echo ""
+  echo "* If you don't want the split after all, "
+  echo "reset your branch to what it was before"
+  echo ""
+  echo ("  $ git reset --hard " <> currentShort)
